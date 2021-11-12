583cc2ff7ce1f41141288b28ec316c0e3451da3d
ProElim <elimorellanae@gmail.com>
2021-11-11 22:55:28 -0400

    cambio de direccion de la base de datos

--- src/ConectarBD.java
+++ src/ConectarBD.java
@@ -1,25 +1,23 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+
 public class ConectarBD {
 
+    public static final String URL = "jdbc:mysql://broxmlmxhcp1au0rycty-mysql.services.clever-cloud.com:3306/broxmlmxhcp1au0rycty";
+    public static final String USER = "ug3ik4c1a2nb7o8d";
+    public static final String CLAVE = "uAiugnzOXmOkIMjUPgwo";
 
-  
-    public static final String URL = "jdbc:mysql://bbid848puqylxajofwmn-mysql.services.clever-cloud.com:3306/bbid848puqylxajofwmn";
-    public static final String USER = "ubnaz9ilslowrglt";
-    public static final String CLAVE = "sQWJXFBLojALuA6NS9f5";
-     
-    public Connection getConexion(){
+    public Connection getConexion() {
         Connection con = null;
-        try{
-            //Class.forName("com.mysql.cj.jdbc.Driver");
+        try {
+            // Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(URL, USER, CLAVE);
             System.out.println("conectado con exito");
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
         return con;
     }
-
 
 }
