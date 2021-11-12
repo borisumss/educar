3097f54c50f257b364b4d9691c7fae198c41c202
ProElim <elimorellanae@gmail.com>
2021-11-11 22:45:19 -0400

    creación ventana inicial

--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
 import ventanas.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
- 
-        Home l=new Home();
-        l.setVisible(true);
+
+        Principal prin = new Principal();
+        prin.setVisible(true);
+        // Home l = new Home();
+        // l.setVisible(true);
     }
 }
